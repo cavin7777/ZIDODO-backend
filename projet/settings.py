bc4031f24fa3906005cd1a36807f4b09dff29355
cavin7777 <caven7777@gameil.com>
2026-03-03 09:59:40 +0400

    version_0.3

--- projet/settings.py
+++ projet/settings.py
@@ -24,12 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
-
     'myapp',   # Your app
 ]
-
 
 # ==========================================
 # MIDDLEWARE
